661fa0ebd4e05dcfc6d21dc273fdff12b6c093a2
Raji Oluwafunbi <roluwafunbi@gmail.com>
2022-03-23 20:30:45 +0100

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,16 +1,19 @@
-const path = require("path");
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      username: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA", "public"),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
